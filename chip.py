e68b6e788a262b1906e85287a5edf9d355df12a1
Anthony Du <github@anthonydu.com>
2023-10-01 03:16:18 -0700

    Update!!!

--- chip.py
+++ chip.py
@@ -1,16 +1,27 @@
 import sys
+import logging
 import re
 import json
-import chinese2digits as c2d
+import cn2an
+from io import StringIO
+from contextlib import redirect_stdout
 
 
 def interpret(file):
     content = open(file, "r").read()
+    stringLiterals = re.findall("「(.*?)」", content)
+    for s in stringLiterals:
+        content.replace(s, "「」")
     for k, v in json.loads(open("dict.json", "r").read()).items():
         content = re.sub(k, v, content)
-    content = c2d.takeChineseNumberFromString(content)["replacedText"]
+    content = cn2an.transform(content, "cn2an")
+    for s in stringLiterals:
+        content.replace("「」", f"「{s}」", 1)
     return content
 
 
 if __name__ == "__main__":
-    exec(interpret(sys.argv[1]))
+    o = StringIO()
+    with redirect_stdout(o):
+        exec(interpret(sys.argv[1]))
+    print(cn2an.transform(o.getvalue(), "an2cn"), end="")
