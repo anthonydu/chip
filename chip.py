5db84a638efa23901c6c2ca0a750ce84d5506e76
Anthony Du <github@anthonydu.com>
2023-10-01 03:29:27 -0700

    Bug fixes

--- chip.py
+++ chip.py
@@ -1,5 +1,4 @@
 import sys
-import logging
 import re
 import json
 import cn2an
@@ -11,12 +10,12 @@ def interpret(file):
     content = open(file, "r").read()
     stringLiterals = re.findall("「(.*?)」", content)
     for s in stringLiterals:
-        content.replace(s, "「」")
+        content = content.replace(s, "�")
     for k, v in json.loads(open("dict.json", "r").read()).items():
         content = re.sub(k, v, content)
     content = cn2an.transform(content, "cn2an")
     for s in stringLiterals:
-        content.replace("「」", f"「{s}」", 1)
+        content = content.replace("�", f"{s}", 1)
     return content
 
 
@@ -25,3 +24,4 @@ if __name__ == "__main__":
     with redirect_stdout(o):
         exec(interpret(sys.argv[1]))
     print(cn2an.transform(o.getvalue(), "an2cn"), end="")
+    # print(interpret(sys.argv[1]))
